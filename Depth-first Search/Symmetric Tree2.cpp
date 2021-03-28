d0cba178c37724c3bc0ecd4d7dfb538954160e9f
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-03-28 23:34:51 +0530

    Create Symmetric Tree2.cpp

---
+++ Depth-first Search/Symmetric Tree2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkBST(TreeNode*l,TreeNode*r){
+        if(l==NULL&&r==NULL)
+                return true;
+        if(r!=NULL&&l!=NULL)
+        {
+                if(r->val==l->val)
+                        return checkBST(l->left,r->right)&&checkBST(l->right,r->left);
+                else
+                        return false;
+        }
+        else return false;
+}
+    bool isSymmetric(TreeNode* root) {
+         if(root==NULL)
+                return true;
+        return checkBST(root->left,root->right);
+    }
+};
