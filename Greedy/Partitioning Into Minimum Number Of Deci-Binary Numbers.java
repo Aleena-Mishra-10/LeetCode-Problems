bdbb29dc0ef15e29f595174fc6902894a9a2e5e3
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-06-06 11:00:46 +0530

    Create Partitioning Into Minimum Number Of Deci-Binary Numbers.java

---
+++ Greedy/Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minPartitions(String n) {
+        int maxDigit = 0;
+        for(char digit : n.toCharArray()){
+            maxDigit = Math.max(maxDigit,digit - '0');
+            if(maxDigit == 9) break;
+        }
+        return maxDigit;
+    }
+}
