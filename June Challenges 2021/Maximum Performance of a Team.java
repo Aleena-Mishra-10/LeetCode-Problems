8c14100b254df49c9b1ecd490f91a872d5cb15d7
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-06-06 10:35:20 +0530

    Update Maximum Performance of a Team.java

--- June Challenges 2021/Maximum Performance of a Team.java
+++ June Challenges 2021/Maximum Performance of a Team.java
@@ -1,18 +1,55 @@
+// Sort incoming values in descending order of efficiency
+// Why? This way whenever you are calculating answer for
+// current state then you will have minimum efficiency for 
+// that state with you , if the number of engineers exceeds k
+// then remove the engineer with least speed from current state 
+// this works because efficiency of the group is fixed with coming engineer 
+// and the only thing that matters for performance now is speed
+
+// TC : O(nlogn)
+// SC : O(n)
+
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[][] ord = new int[n][2];
-        for (int i = 0; i < n; i++)
-            ord[i] = new int[] {efficiency[i], speed[i]};
-        Arrays.sort(ord, (a, b) -> Integer.compare(b[0], a[0]));
-        PriorityQueue<Integer> sppq = new PriorityQueue<>();
-        long totalSpeed = 0, best = 0;
-        for (int[] pair : ord) {
-            int spd = pair[1];
-            sppq.add(spd);
-            if (sppq.size() <= k) totalSpeed += spd;
-            else totalSpeed += spd - sppq.poll();
-            best = Math.max(best, totalSpeed * pair[0]);
+        Engineer arr[] = new Engineer[n];
+        for(int i=0;i<n;i++) arr[i] = new Engineer(speed[i],efficiency[i]);
+        Arrays.sort(arr); // sort in descending order of efficiency
+        int m = 1000000007; // to avoid overflow 
+        PriorityQueue<Integer> pq = new PriorityQueue(); // min heap so that we
+        // can remove engineer with lowest speed from group if group size>k
+        long totalSpeed = 0;
+        long ans = Long.MIN_VALUE;
+        
+        for(Engineer engineer : arr){
+            if(pq.size()<k){ 
+                pq.add(engineer.speed);
+                totalSpeed+=engineer.speed;
+            }else{ // size will exceed k if current engineer becomes 
+                // part of group so remove engineer which has slowest speed
+                // from the group
+                totalSpeed-=pq.remove();
+                pq.add(engineer.speed);
+                totalSpeed+=engineer.speed;
+            }
+            ans = Math.max(ans,totalSpeed*engineer.efficiency); // if answer for current state
+            // exceeds answer so far then update answer so far
         }
-        return (int)(best % 1000000007);
+        
+        return (int)(ans%m);
+    }
+}
+class Engineer implements Comparable<Engineer>{
+    int speed;
+    int efficiency;
+    
+    public Engineer(int speed , int efficiency){
+        this.speed=speed;
+        this.efficiency = efficiency;
+    }
+    
+    public int compareTo(Engineer other){
+        if(this.efficiency>other.efficiency) return -1;
+        else if(this.efficiency<other.efficiency) return 1;
+        return 0;
     }
 }
