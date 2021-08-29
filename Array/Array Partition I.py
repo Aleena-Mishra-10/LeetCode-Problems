489ba61e9ea332941f9b3191dfc7d89f32b41c94
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-08-29 12:46:19 +0530

    Create Array Partition I.py

---
+++ Array/Array Partition I.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums , s =  sorted(nums),0
+        for i in range(0,len(nums),2):
+            s += nums[i]
+        return s
